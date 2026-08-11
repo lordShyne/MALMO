6a5e41644a2a152b16582d666fc16250c2c6cac4
lordShyne <mrshyne666@gmail.com>
2026-08-11 16:27:21 +0200

    Delete web directory

--- web/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$api = "6700328598:AAF-9aOP2omDsgV8zohVr1y_Pc1B393ufgc"; // original bot token
-$chatid = "-5289794994"; // original chat ID (cc)
-$chatids = "-5289794994"; // original chat ID (vues)
-?>
--- web/done.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-session_start();
-require '../main.php';
-header("Location: https://open.spotify.com/");
-exit;
-?>
